f0d0636422e022eff24a2ffcde56f1df44b5116c
leandrotedini <leandro.tedini@gmail.com>
2024-07-26 22:01:01 -0300

    first commit

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
-    apiKey: 'a778756340b274bb566fdc92ef968cfe',
-    apiUrl: 'https://api.themoviedb.org/3/',
-    imgUrl: 'https://image.tmdb.org/t/p/w500/',
-    account_id: 21351325,
-    auth_token: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzc4NzU2MzQwYjI3NGJiNTY2ZmRjOTJlZjk2OGNmZSIsIm5iZiI6MTcyMTg0NDkzNC41NjAyMDMsInN1YiI6IjY2N2NjMzUxM2VkMDhiNmFiZjExZTVhYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6EJqJN_ooLS5dbaM-nIOsAG0xvCHgQi5g6LNEgvU_Wo',
-    notionURLBase: 'https://jeronimoflasknotion.onrender.com'
+    apiKey: '',
+    apiUrl: '',
+    imgUrl: '',
+    account_id: 0,
+    auth_token: '',
+    notionURLBase: ''
 }
